830023891ce5177b4faa4f960dc182fe2b29aa9c
William Berry <76190311+wberry86@users.noreply.github.com>
2021-04-27 06:23:00 -0500

    finalized refactor

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,19 +2,19 @@ const path = require('path');
 const router = require('express').Router();
 
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'))
   });
   
-  app.get('/animals', (req, res) => {
+  router.get('/animals', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/animals.html'));
   });
   
-  app.get('/zookeepers', (req, res) => {
+  router.get('/zookeepers', (req, res) => {
     res.sendFile(path.join(__dirname,'../../public/zookeepers.html'));
   });
   
-  app.get('*', (req, res) => {
+  router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
   });
 
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const fs = require('fs');
-const path = require('path');
 const express = require("express");
-const { animals } = require("./data/animals.json");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-app.use(express.static('public'));
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
 
 
 
