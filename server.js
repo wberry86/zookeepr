9f8bef2ed659309e2cf6860049af3148851d2f7b
William Berry <76190311+wberry86@users.noreply.github.com>
2021-04-26 19:57:24 -0500

    updated getAnimals function

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -101,6 +102,22 @@ app.post("/api/animals", (req, res) => {
 
   res.json(req.body);
   }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 
